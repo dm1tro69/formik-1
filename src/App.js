fe106a5ce45872e5985f12720ac429c75885feb5
dm1tro69 <dimolg22@gmail.com>
2022-04-30 21:46:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import rocket from './assets/rocket.png'
+import Signup from "./components/Signup";
+
+function App() {
+  return (
+    <div className={'container mt-3'}>
+    <div className={'row'}>
+      <div className={'col-md-5'}>
+        <Signup/>
+      </div>
+      <div className={'col-md-7'}>
+        <img className={'img-fluid w-100'} src={rocket} alt="img"/>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
