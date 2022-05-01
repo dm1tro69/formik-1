6dfbdeffd7412b775084821be7f2b85362f4cb4c
dm1tro69 <dimolg22@gmail.com>
2022-05-01 20:25:18 +0300

    finished

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import {Formik, Form} from "formik";
+import TestField from "./TestField";
+import * as Yup from 'yup'
 
 const Signup = () => {
+    const validate = Yup.object({
+        firstName: Yup.string()
+            .max(15, 'Must be 15 character or less')
+            .required('Required'),
+        lastName: Yup.string()
+            .max(20, 'Must be 20 character or less')
+            .required('Required'),
+        email: Yup.string()
+            .email('Email is invalid')
+            .required('Required'),
+        password: Yup.string()
+            .min(6, 'Password must be at least 6 characters')
+            .required('Password is required'),
+        confirmPassword: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Password must match')
+            .required('Confirm password is required')
+    })
     return (
-        <Formik>
+        <Formik
+            initialValues={{
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            }}
+            validationSchema={validate}
+            onSubmit={values => {
+                console.log(values)
+            }}
+        >
             {formik => (
                 <div>
                     <h1 className={'my-4 font-weight-bold-display-4'}>Sign Up</h1>
+                    <Form>
+                       <TestField label={'First Name'} name={'firstName'} type={'text'}/>
+                       <TestField label={'Last Name'} name={'lastName'} type={'text'}/>
+                       <TestField label={'Email'} name={'email'} type={'email'}/>
+                       <TestField label={'password'} name={'password'} type={'password'}/>
+                       <TestField label={'Confirm password'} name={'confirmPassword'} type={'password'}/>
+                        <button className={'btn btn-dark mt-3'} type={'submit'}>Register</button>
+                        <button className={'btn btn-danger mt-3 ml-3'} type={'reset'}>Reset</button>
+                    </Form>
                 </div>
             )}
         </Formik>
